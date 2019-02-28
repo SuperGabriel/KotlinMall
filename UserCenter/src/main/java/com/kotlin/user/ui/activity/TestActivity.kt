629ecb9b0fbff40d9dbc22dec13f7510a7169d1d
yh-Gabriel <214524224@qq.com>
2019-02-28 16:56:09 +0800

    第二章 00:40:31

--- UserCenter/src/main/java/com/kotlin/user/ui/activity/TestActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/TestActivity.kt
@@ -3,11 +3,30 @@ package com.kotlin.user.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.user.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+//        setContentView(R.layout.activity_test)
+        verticalLayout {
+            padding = 30
+            editText{
+                hint="Name"
+                textSize=24f
+            }
+            editText{
+                hint="Pwd"
+                textSize=24f
+            }
+            button{
+                text="Test"
+                onClick {
+                    toast("${intent.getIntExtra("id",-1)}")
+                }
+            }
+        }
     }
 }
